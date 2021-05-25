fe8fe283bc5e74b7908648058e97b17762c748eb
Pedro <pedrochatelain@gmail.com>
2021-05-25 15:19:15 -0300

    Change html to ejs templates

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
 
 app.set('views', path.join(__dirname, 'views'))
 
+app.set('view engine', 'ejs')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(require('./routes.js'))
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
-router.get('/', (req, res) => res.sendFile(__dirname + '/views/home.html'))
+router.get('/', (req, res) => res.render('home.ejs'))
 
-router.get('/add_movies', (req, res) => res.sendFile(__dirname + '/views/add_movies.html'))
+router.get('/add_movies', (req, res) => res.render('add_movies.ejs'))
 
-router.get('/my_movies', (req, res) => res.sendFile(__dirname + '/views/my_movies.html'))
+router.get('/my_movies', (req, res) => res.render('my_movies.ejs'))
 
 module.exports = router
