30bab00f5b7ba1222661ac547085fc085ddcf18a
Pedro <pedrochatelain@gmail.com>
2021-06-29 16:05:04 -0300

    Remove delete card method

--- src/model.js
+++ src/model.js
@@ -33,12 +33,6 @@ async function setRating(rating, id) {
   }
 }
 
-function deleteAllMovies() {
-  pool.query('DELETE FROM moviesv2');
-}
-
-// deleteAllMovies();
-
 async function getMovies() {
   const query = await pool.query(
    `SELECT id, name, director, TO_CHAR(date, 'DD-MM-YYYY') AS date, rating, image
